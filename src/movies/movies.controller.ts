bd443a64befdbd9bc446d6bb12f93cb991fad3f1
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-11-10 00:07:38 +0900

    add kind of Deroator as Body, Query

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,23 +16,31 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get('/:id')
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `we are searching for movie made after : ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id : ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will creaet a movie';
+  create(@Body() movieData) {
+    return `This will creaet a movie, body : ${movieData}`;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id : ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
