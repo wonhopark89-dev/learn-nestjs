9dce05a6a4c65270f5f16f82efe16015c92d1ce6
wonhopark89-dev <wonhopark89.dev@gmail.com>
2020-12-06 18:59:35 +0900

    how to check validation with input

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, // 목록에 없는것은 받지 않음
+      transform: true, // 요청하는 값을 변환할 수 있음
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
